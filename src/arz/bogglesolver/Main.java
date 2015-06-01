6ecfd74740a2c9d743300ee5fc7176158414eabb
arzman <arz.man@free.fr>
2015-06-01 09:49:11 +0100

    tets

--- src/arz/bogglesolver/Main.java
+++ src/arz/bogglesolver/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Troisième version de la classe");
+		System.out.println("Quatrième version de la classe");
 
 	}
 
