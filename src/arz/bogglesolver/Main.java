167c7173ee169ae9f4a155f3af8eba10edaaa524
arzman <arz.man@free.fr>
2015-06-01 09:47:52 +0100

    test

--- src/arz/bogglesolver/Main.java
+++ src/arz/bogglesolver/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Deuxieme version de la classe");
+		System.out.println("Troisième version de la classe");
 
 	}
 
