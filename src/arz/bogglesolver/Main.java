20b1dd83828694ac10227f9524420dee578f201c
arzman <arz.man@free.fr>
2015-06-01 09:52:53 +0100

    lol

--- src/arz/bogglesolver/Main.java
+++ src/arz/bogglesolver/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Quatrième version de la classe");
+		System.out.println("Cinquième version de la classe");
 
 	}
 
